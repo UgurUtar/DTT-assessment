f4d9ab7e845f0ee0bbd4cae2b7b6b583856da12b
UgurUtar <ugur-2003@live.nl>
2023-02-21 15:36:59 +0100

    Added API fetch to ListingHome

--- src/components/Home/ListingHome.vue
+++ src/components/Home/ListingHome.vue
@@ -1,52 +1,81 @@
 <script>
+const myHeaders = new Headers();
+myHeaders.append("X-Api-Key", "9alQkyY7gWw-L4RDVAsZHvBxiKtdoh05");
+
+const requestOptions = {
+    method: 'GET',
+    headers: myHeaders,
+    redirect: 'follow'
+};
 export default {
     data() {
-      return {
-        houses: [
-          { id: 1, street: "Johan Kernstraat 17", price: "€ 200.000", postalcode: "1063VC Amsterdam", beds: "3", baths: "1", size: "100 m2" },
-          { id: 2, street: "Johan Kernstraat 17", price: "€ 200.000", postalcode: "1063VC Amsterdam", beds: "3", baths: "1", size: "100 m2" },
-          { id: 3, street: "Johan Kernstraat 17", price: "€ 200.000", postalcode: "1063VC Amsterdam", beds: "3", baths: "1", size: "100 m2" }
-        ]
-      };
-    }
-};
-</script>
+        return {
+            houses: [],
+        };
+    },
+    methods: {
+        getHouses() {
 
+            fetch("https://api.intern.d-tt.nl/api/houses", requestOptions)
+                .then(response => response.json())
+                .then(data => this.houses = data)
+        },
+        deleteHouse(id) {
+            const deleteOptions = {
+                method: 'DELETE',
+                headers: myHeaders,
+                redirect: 'follow'
+            };
+            fetch("https://api.intern.d-tt.nl/api/houses/${id}", deleteOptions)
+                .then(response => response.text())
+                .then(result => {
+                    console.log(result)
+                    this.getHouses()
+                })
+                .catch(error => console.log('error', error));
+        }
+
+    },
+
+    mounted() {
+        this.getHouses()
+    }
+}
+</script>
 <template>
     <div class="wrapper">
         <div class="house" v-for="house in houses" :key="house.id">
             <RouterLink class="housedetail" :to="`/home/detail/${house.id}`">
-            <div class="houseimage">
-                <img class="houseimg" src="@/assets/dtt/img_placeholder_house.png" alt="house">
-            </div>
-            <div class="inforow">
-                <a class="street">{{ house.street }}</a>
-                <a class="price">{{ house.price }}</a>
-                <a class="postalcode">{{ house.postalcode }}</a>
-                <div class="houseinfo">
-                    <img class="bed" src="@/assets/dtt/ic_bed.png" alt="bed">
-                    <a class="beds">{{ house.beds }}</a>
-                    <img class="bath" src="@/assets/dtt/ic_bath.png" alt="bath">
-                    <a class="baths">{{ house.baths }}</a>
-                    <img class="size" src="@/assets/dtt/ic_size.png" alt="size">
-                    <a class="sizes">{{ house.size }}</a>
+                <div class="houseimage">
+                    <img class="houseimg" :src="`${house.image}`" alt="house">
                 </div>
-            </div>
-            <div class="edits">
-                <RouterLink class="edit" to="/edit-house">
-                    <img class="edit" src="@/assets/dtt/ic_edit.png" alt="edit">
-                </RouterLink>
-                <RouterLink class="delete" to="/delete">
-                    <img class="delete" src="@/assets/dtt/ic_delete.png" alt="delete">
-                </RouterLink>
-            </div>
-        </RouterLink>
+                <div class="inforow">
+                    <a class="street">{{ house.location.street }}</a>
+                    <a class="price">€ {{ house.price }}</a>
+                    <a class="postalcode">{{ house.location.zip + ' ' + house.location.city }}</a>
+                    <div class="houseinfo">
+                        <img class="bed" src="@/assets/dtt/ic_bed.png" alt="bed">
+                        <a class="beds">{{ house.rooms.bedrooms }}</a>
+                        <img class="bath" src="@/assets/dtt/ic_bath.png" alt="bath">
+                        <a class="baths">{{ house.rooms.bathrooms }}</a>
+                        <img class="size" src="@/assets/dtt/ic_size.png" alt="size">
+                        <a class="sizes">{{ house.size }} m2</a>
+                    </div>
+                </div>
+                <div class="edits">
+                    <RouterLink class="edit" to="/edit-house">
+                        <img class="edit" src="@/assets/dtt/ic_edit.png" alt="edit">
+                    </RouterLink>
+                    <RouterLink class="delete" @click="deleteHouse(house.id)" :to="`/home/detail/${house.id}/delete`">
+                        <img class="delete" src="@/assets/dtt/ic_delete.png" alt="delete">
+                    </RouterLink>
+                </div>
+            </RouterLink>
         </div>
     </div>
 </template>
 
 <style scoped>
-
 .wrapper {
     display: flex;
     flex-direction: column;
@@ -75,7 +104,7 @@ export default {
     margin: 6px;
 }
 
-.houseimage img  {
+.houseimage img {
     width: 140px;
     height: 140px;
     border-radius: 5px;
@@ -94,19 +123,22 @@ export default {
     font-weight: 550;
     font-size: 18px;
     color: var(--primary-text);
+    margin-top: 8px;
 }
 
 .price {
     color: var(--secondary);
+    margin-top: 5px;
 }
 
 .postalcode {
     color: var(--tertiary2);
+    margin-top: 5px;
 }
 
 .houseinfo {
     height: 10%;
-    margin-top: 5px;
+    margin-top: 10px;
     padding-right: 10px;
     width: 30%;
     display: flex;
@@ -117,7 +149,7 @@ export default {
     font-family: var(--secondary-text);
     font-size: 14px;
     white-space: nowrap;
-    margin-right: 10px;
+    margin: -3px 13px 0px 2px;
 }
 
 .edits {
@@ -134,90 +166,92 @@ export default {
 
 @media screen and (max-width: 750px) {
     .wrapper {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    margin-top: 30px;
-    width: 90%;
-    margin-left: 5%;
-}
-.edits {
-    position: absolute;
-    right: 0;
-    display: flex;
-    height: 25px;
-    padding: 4px;
-    margin-right: 10px;
-}
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        margin-top: 30px;
+        width: 90%;
+        margin-left: 5%;
+    }
+
+    .edits {
+        position: absolute;
+        right: 0;
+        display: flex;
+        height: 25px;
+        padding: 4px;
+        margin-right: 10px;
+    }
 
 }
 
 @media screen and (max-width: 500px) {
     .wrapper {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    margin-top: 30px;
-    width: 90%;
-    margin-left: 5%;
-}
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        margin-top: 30px;
+        width: 90%;
+        margin-left: 5%;
+    }
 
-.house {
-    display: flex;
-    background-color: var(--background2);
-    border: solid 0 var(--tertiary);
-    border-radius: 5px;
-    height: 120px;
-    width: 100%;
-    padding: 10px;
-}
+    .house {
+        display: flex;
+        background-color: var(--background2);
+        border: solid 0 var(--tertiary);
+        border-radius: 5px;
+        height: 120px;
+        width: 100%;
+        padding: 10px;
+    }
 
-.street {
-    font-weight: 550;
-    font-size: 12px;
-    color: var(--primary-text);
-    width: 65%;
-}
+    .street {
+        font-weight: 550;
+        font-size: 12px;
+        color: var(--primary-text);
+        width: 65%;
+    }
 
-.inforow {
-    display: flex;
-    flex-direction: column;
-    flex: 1;
-    font-weight: 300;
-    font-size: 12px;
-    width: 67%;
-}
+    .inforow {
+        display: flex;
+        flex-direction: column;
+        flex: 1;
+        font-weight: 300;
+        font-size: 12px;
+        width: 67%;
+    }
 
-.houseinfo {
-    height: 10%;
-    margin-top: 18px;
-    width: 90%;
-    height: 15px;
-}
+    .houseinfo {
+        height: 10%;
+        margin-top: 18px;
+        width: 90%;
+        height: 15px;
+    }
 
-.houseimage img  {
-    width: 100px;
-    height: 100px;
-    border-radius: 5px;
-    object-fit: cover;
-    object-position: left;
-}
+    .houseimage img {
+        width: 100px;
+        height: 100px;
+        border-radius: 5px;
+        object-fit: cover;
+        object-position: left;
+    }
 
-.edits {
-    position: absolute;
-    right: 0;
-    display: flex;
-    height: 20px;
-    padding: 4px;
-}
-.edit {
-    display: flex;
-}
+    .edits {
+        position: absolute;
+        right: 0;
+        display: flex;
+        height: 20px;
+        padding: 4px;
+    }
 
-.delete {
-    display: flex;
-}
+    .edit {
+        display: flex;
+    }
+
+    .delete {
+        display: flex;
+    }
 }
 </style>
