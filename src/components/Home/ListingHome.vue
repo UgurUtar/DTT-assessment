b1995d8923767b195cfe02e95530df01323420ff
UgurUtar <ugur-2003@live.nl>
2023-03-14 00:20:00 +0100

    Added pinia and used pinia for every vue component

--- src/components/Home/ListingHome.vue
+++ src/components/Home/ListingHome.vue
@@ -1,50 +1,87 @@
 <script>
-const myHeaders = new Headers();
-myHeaders.append("X-Api-Key", "9alQkyY7gWw-L4RDVAsZHvBxiKtdoh05");
+import { useHousesStore } from '@/stores/app.js';
+import { computed } from 'vue';
+import { searchQuery } from '@/components/Home/Home.vue';
 
-const requestOptions = {
-    method: 'GET',
-    headers: myHeaders,
-    redirect: 'follow'
-};
 export default {
-    data() {
+    setup() {
+        const storeHouses = useHousesStore();
+        const getHouses = () => {
+            storeHouses.getHouses();
+        };
+
+        const deleteHouse = (id) => {
+            storeHouses.deleteHouse(id);
+        };
+
+        const filteredHouses = computed(() => {
+            if (!searchQuery.value) {
+                return storeHouses.houses;
+            }
+            const lowerCaseQuery = searchQuery.value.toLowerCase();
+
+            return storeHouses.houses.filter(house =>
+                house.location.street.toLowerCase().includes(lowerCaseQuery) ||
+                house.location.city.toLowerCase().includes(lowerCaseQuery) ||
+                house.location.zip.toLowerCase().includes(lowerCaseQuery) ||
+                house.price.toString().toLowerCase().includes(lowerCaseQuery) ||
+                house.size.toString().toLowerCase().includes(lowerCaseQuery)
+            );
+        });
+
+        const deletePopup = (id) => {
+            const popup = document.getElementById('popup');
+            popup.style.display = 'flex';
+            const yes = document.getElementById('yes');
+            yes.addEventListener('click', () => {
+                deleteHouse(id);
+                popup.style.display = 'none';
+            });
+            const no = document.getElementById('no');
+            no.addEventListener('click', () => {
+                popup.style.display = 'none';
+            });
+        };
+
+
+
         return {
-            houses: [],
+            searchQuery,
+            getHouses,
+            filteredHouses,
+            deleteHouse,
+            deletePopup,
         };
     },
-    methods: {
-        getHouses() {
 
-            fetch("https://api.intern.d-tt.nl/api/houses", requestOptions)
-                .then(response => response.json())
-                .then(data => this.houses = data)
-        },
-        deleteHouse(id) {
-            const deleteOptions = {
-                method: 'DELETE',
-                headers: myHeaders,
-                redirect: 'follow'
-            };
-            fetch("https://api.intern.d-tt.nl/api/houses/${id}", deleteOptions)
-                .then(response => response.text())
-                .then(result => {
-                    console.log(result)
-                    this.getHouses()
-                })
-                .catch(error => console.log('error', error));
-        }
-
-    },
-
-    mounted() {
-        this.getHouses()
+    created() {
+        this.getHouses();
     }
-}
+};
 </script>
 <template>
     <div class="wrapper">
-        <div class="house" v-for="house in houses" :key="house.id">
+        <div class="popup" id="popup">
+            <div class="popup-content">
+                <a class="headertext">Delete Listing</a>
+                <a class="popup-text" id="firsttext">Are you sure you want to delete this listing?</a>
+                <a class="popup-text" id="secondtext">This action cannot be undone.</a>
+                <div class="popup-buttons">
+                    <button class="popup-button" id="yes">YES, DELETE</button>
+                    <button class="popup-button" id="no">GO BACK</button>
+                </div>
+            </div>
+        </div>
+        <div class="total" v-if="searchQuery && filteredHouses.length">
+            <a class="totaltext">{{ filteredHouses.length }} results found</a>
+        </div>
+        <div class="noresult" v-if="filteredHouses.length === 0">
+            <img class="noresultimg" src="@/assets/dtt/img_empty_houses.png" alt="no result">
+            <a class="noresulttext">No results found.</a>
+            <br>
+            <a class="noresulttexttwo">Please try another keyword.</a>
+        </div>
+        <div class="house" v-for="house in filteredHouses" :key="house.id">
             <RouterLink class="housedetail" :to="`/home/detail/${house.id}`">
                 <div class="houseimage">
                     <img class="houseimg" :src="`${house.image}`" alt="house">
@@ -62,15 +99,15 @@ export default {
                         <a class="sizes">{{ house.size }} m2</a>
                     </div>
                 </div>
-                <div class="edits">
-                    <RouterLink class="edit" to="/edit-house">
-                        <img class="edit" src="@/assets/dtt/ic_edit.png" alt="edit">
-                    </RouterLink>
-                    <RouterLink class="delete" @click="deleteHouse(house.id)" :to="`/home/detail/${house.id}/delete`">
-                        <img class="delete" src="@/assets/dtt/ic_delete.png" alt="delete">
-                    </RouterLink>
-                </div>
             </RouterLink>
+            <div class="edits" v-if="house.madeByMe">
+                <RouterLink class="edit" :to="`/edit-house/${house.id}`">
+                    <img class="edit" src="@/assets/dtt/ic_edit.png" alt="edit">
+                </RouterLink>
+                <div class="deletebutton" @click="deletePopup(house.id)">
+                    <img class="delete" src="@/assets/dtt/ic_delete.png" alt="delete">
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -86,11 +123,127 @@ export default {
     margin-left: 15%;
 }
 
+.deletebutton {
+    width: 15px;
+    cursor: pointer;
+}
+
+#yes, #no {
+    cursor: pointer;
+}
+
+.popup {
+    display: none;
+    position: fixed;
+    z-index: 1;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.4);
+}
+
+.popup-content {
+    background-color: var(--background);
+    padding: 20px;
+    width: 40%;
+    margin: auto;
+    height: 35%;
+    border-radius: 3%;
+}
+
+.headertext {
+    font-family: var(--font-family);
+    font-size: 20px;
+    display: flex;
+    justify-content: center;
+    color: var(--primary-text);
+    font-weight: 550;
+    margin-top: 15px;
+}
+
+.popup-text {
+    font-family: var(--font-family);
+    display: flex;
+    justify-content: center;
+    font-size: 12px;
+    color: var(--secondary-text);
+}
+
+#firsttext {
+    margin-top: 15px;
+}
+
+.popup-buttons {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 100%;
+    height: 100%;
+}
+
+.popup-button {
+    font-family: var(--font-family);
+    width: 50%;
+    height: 15%;
+    margin-top: 20px;
+    border: none;
+    border-radius: 5px;
+    background-color: var(--primary);
+    color: var(--background);
+    font-weight: 550;
+}
+
+#no {
+    background-color: var(--secondary);
+}
+
 .housedetail {
     display: flex;
     width: 100%;
     height: 100%;
     text-decoration: none;
+}
+
+.total {
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    width: 100%;
+    height: 30px;
+    margin-bottom: 10px;
+}
+
+.totaltext {
+    margin-left: 10px;
+    font-size: 18px;
+    color: var(--primary-text);
+    font-weight: 550;
+}
+
+.noresult {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 100%;
+    margin-top: 15%;
+}
+
+.noresultimg {
+    height: 200px;
+}
+
+.noresulttext {
+    font-size: 18px;
+    color: var(--secondary);
+    margin-top: 20px;
+}
+
+.noresulttexttwo {
+    font-size: 18px;
+    color: var(--secondary);
 }
 
 .house {
@@ -173,6 +326,15 @@ export default {
         margin-top: 30px;
         width: 90%;
         margin-left: 5%;
+    }
+
+    .popup-content {
+        background-color: var(--background);
+        padding: 20px;
+        width: 70%;
+        margin: auto;
+        height: 35%;
+        border-radius: 3%;
     }
 
     .edits {
--- src/main.js
+++ src/main.js
@@ -7,8 +7,8 @@ import router from "./router";
 import "./assets/main.css";
 
 const app = createApp(App);
-
-app.use(createPinia());
+const pinia = createPinia();
+app.use(pinia);
 app.use(router);
 
 app.mount("#app");
